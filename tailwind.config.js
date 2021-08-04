9d4cda69b1da6053f3b3bf1f9ccb6e34ba1f1b78
sidiDev <sididev3@gmail.com>
2021-08-04 17:00:30 +0000

    add new folder on tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
   purge: [
     "./pages/**/*.{js,jsx,ts,tsx}",
     "./components/**/*.{js,jsx,ts,tsx}",
+    "./dashboard/**/*.{js,jsx,ts,tsx}",
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
