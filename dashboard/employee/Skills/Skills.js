949b6a3d2ce9e5a941c9a18938aeb70cc9c7b1ca
sidiDev <sididev3@gmail.com>
2021-08-04 18:06:48 +0000

    create skills page

--- dashboard/employee/Skills/Skills.js
+++ dashboard/employee/Skills/Skills.js
@@ -12,24 +12,42 @@ export default () => {
         console.log(inputValue);
     }
 
+    // Remove skill from client side
+    const rmSkillFromCs = (idx) => {
+
+        const rmSkill = newSkills.filter(( skill, index ) => {
+            return index != idx
+        })
+
+        setNewSkill(rmSkill)
+
+    }
+
     return (
         <div className="col-span-2 max-w-lg">
             <h1 className="font-semibold text-gray-800 text-2xl">Skills</h1>
-            <div className="flex flex-wrap mt-4 gap-3">
-                <span className="inline-block px-3 py-2 rounded bg-gray-100 text-gray-700">
+            <div className="flex flex-wrap mt-4">
+                <span className="inline-block mr-3 mt-3 px-3 py-2 rounded bg-gray-100 text-gray-700">
                     JavaScript
                 </span>
                 {
                     newSkills.map(( items, idx ) => {
                         return (
-                            <span key={ idx } className="inline-block px-3 py-2 rounded bg-gray-100 text-gray-700">
+                            <span key={ idx } className="inline-flex mr-3 mt-3 px-3 py-2 rounded bg-gray-100 text-gray-700">
                                 { items }
+                                <button className="text-gray-500 mx-1"
+                                    onClick={() => rmSkillFromCs(idx)}
+                                >
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
+                                        <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
+                                    </svg>
+                                </button>
                             </span>
                         )
                     })
                 }
             </div>
-            <SkillsForm addKill={ addKill } onInput={ setInputValue } inputValue={ inputValue }  />
+            <SkillsForm addKill={ addKill } onInput={ setInputValue } skills={newSkills} inputValue={ inputValue }  />
         </div>
     )
 }
--- dashboard/employee/Skills/SkillsForm.js
+++ dashboard/employee/Skills/SkillsForm.js
@@ -1,15 +1,23 @@
 import Input from "../../../components/Input/Input"
 
-export default ({ addKill, onInput, inputValue }) => {
+export default ({ addKill, onInput, inputValue, skills }) => {
     return (
         <div className="mt-5">
-            <form>
+            <form onSubmit={(e) => {
+                e.preventDefault();
+                addKill()
+            } }>
                 <Input defaultValue={ inputValue } onInput={(e) => onInput(e.target.value) } type="text" labelName="Add new skill" />
-                <button type="button" className="mt-3 w-full py-2 rounded-md bg-cyan-600 text-white m-auto outline-none shadow-md ring-cyan-600 ring-offset-2 focus:ring-2"
-                    onClick={ addKill }
-                >
-                    Add
-                </button>
+                <div className="flex space-x-3">
+                    <button type="button" className={`${inputValue ? 'bg-cyan-600 text-white shadow-md ring-cyan-600 ring-offset-2 focus:ring-2' : 'text-gray-500 cursor-not-allowed'} mt-3 w-full py-2 rounded-md border m-auto outline-none`}
+                        onClick={ addKill }
+                    >
+                        Add
+                    </button>
+                    <button  type="submit" className={`${skills.length > 0 ? 'bg-cyan-600 text-white shadow-md ring-cyan-600 ring-offset-2 focus:ring-2' : 'bg-gray-200 text-gray-600 cursor-not-allowed'} rounded-md mt-3 w-full py-2 m-auto outline-none`}>
+                        Update
+                    </button>
+                </div>
             </form>
         </div>
     )
