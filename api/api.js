0de1f94faf49cb3c81dd6b7595bb400d2b120819
sidiDev <sididev3@gmail.com>
2021-08-14 11:13:07 +0000

    Fix bug

--- api/api.js
+++ api/api.js
@@ -1,5 +1,3 @@
-import { env } from "../next.config"
-
-const api = env.API || 'http://localhost:8000'
+const api = process.env.API || 'http://localhost:8000'
 // 'https://job-search-api-v1.herokuapp.com'
 export default api
