bdb557e1102e0d0016527a60707f6d34b80d7f00
sidiDev <sididev3@gmail.com>
2021-08-17 14:54:04 +0000

    change api url

--- api/api.js
+++ api/api.js
@@ -1,4 +1,4 @@
-const api = 'http://localhost:8000'
+const api = 'https://job-search-api-v1.herokuapp.com'
 //'http://localhost:8000'
 //'https://job-search-api-v1.herokuapp.com'
 export default api
