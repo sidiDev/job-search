5a16f638e76747448169b9c49744a2f04d14289b
sidiDev <sididev3@gmail.com>
2021-08-17 14:48:23 +0000

    customize apply button

--- components/JobDetails/JobDetails.js
+++ components/JobDetails/JobDetails.js
@@ -3,10 +3,14 @@ import Details from "./Details/Details"
 
 export default ({ data, job }) => {
     return (
-        <div className="col-span-2">
+        <div className="col-span-2 relative">
             <h1 className="hidden text-3xl font-semibold mt-4 mb-7 md:mt-0 md:block">{job.jobTitle}</h1>
-            <Details job={job} />
-            <ApplyBtn data={data} w="w-full sm:px-4 sm:w-auto md:hidden" />
+            <div className="pb-6 sm:mb-0">
+                <Details job={job} />
+            </div>
+            <div className="fixed left-0 right-0 mx-4 bottom-2 sm:static">
+                <ApplyBtn data={data} w="w-full sm:mx-4 sm:w-auto sm:px-12 md:hidden" />
+            </div>
         </div>
     )
 }
