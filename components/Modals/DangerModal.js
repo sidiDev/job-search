33674243e884e729673ab607a60ff0dddb6bb83d
sidiDev <sididev3@gmail.com>
2021-08-18 19:07:11 +0000

    some customize in modals

--- components/Modals/DangerModal.js
+++ components/Modals/DangerModal.js
@@ -1,7 +1,7 @@
 export default ({ p, submitP, headerP, setState, submit }) => {
     return (
-        <div className="fixed top-12 z-20 inset-0 overflow-y-auto" aria-labelledby="modal-title" role="dialog" aria-modal="true">
-            <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+        <div className="fixed top-12 z-20 inset-0" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+            <div className="flex items-end justify-center pt-4 px-4 pb-20 text-center sm:block sm:p-0">
                 <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" aria-hidden="true" onClick={() => setState(false)}></div>
 
                 <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
