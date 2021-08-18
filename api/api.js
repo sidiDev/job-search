afb302a3e8b05351fe80ef0c5128e67bbf28a534
sidiDev <sididev3@gmail.com>
2021-08-18 19:09:19 +0000

    use env

--- api/api.js
+++ api/api.js
@@ -1,4 +1,2 @@
-const api = 'https://job-search-api-v1.herokuapp.com'
-//'http://localhost:8000'
-//'https://job-search-api-v1.herokuapp.com'
+const api = process.env.API || 'http://localhost:8000'
 export default api
--- next.config.js
+++ next.config.js
@@ -1,18 +1,5 @@
-const withPWA = require("next-pwa");
-// const runtimeCaching = require('next-pwa/cache')
-
-// module.exports = {
-//     env: {
-//         API: process.env.API
-//     },
-// }
-
-// module.exports = withPWA({
-//   pwa: {
-//     dest: "public",
-//     register: true,
-//     disable: false,
-//     // runtimeCaching,
-//     sw: '/sw.js'
-//   },
-// });
+module.exports = {
+    env: {
+        API: process.env.API
+    },
+}
